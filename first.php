c6dde37924e16602e8ce5cee0398a71240bb5a3a
LAPTOP-D23MA2SP\ryosuke <ryosuke9913@gmail.com>
2017-05-24 13:51:44 +0900

    練習問題１

--- first.php
+++ first.php
@@ -1,8 +1,8 @@
 <?php
   //練習問題1
   //文字列連結を用いて、自分の名前をブラウザ上に表示する。
-  $first_name = '';
-  $last_name = '';
+  $first_name = 'Ryosuke';
+  $last_name = 'Shibata';
 
   //練習問題2
   //以下の計算をそれぞれ行い、ブラウザ上に表示する。

965194a4f306e217ee29269286807d26260db23d
LAPTOP-D23MA2SP\ryosuke <ryosuke9913@gmail.com>
2017-05-25 10:54:46 +0900

    練習問題２

--- first.php
+++ first.php
@@ -4,19 +4,19 @@
   $first_name = 'Ryosuke';
   $last_name = 'Shibata';
   echo $first_name;
-  echo $last_name;
+  echo $last_name; echo "<br>";
   //練習問題2
   //以下の計算をそれぞれ行い、ブラウザ上に表示する。
   //8 * 3の計算
-
+  echo 8*3;
   //24 / 8の計算
-
+  echo 24/8;
   //6 + 10の計算
-
+  echo 6+10;
   //6 - 10の計算
-
+  echo 6-10;
   //10 % 7の計算
-
+  echo 10 % 7; echo "<br>";
   //練習問題3
   //下記のプログラムのコメント部分をそれぞれ実行し、ブラウザ上に表示する。
   //※自己代入演算子を用いること
